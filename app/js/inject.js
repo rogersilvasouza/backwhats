893f7f1eff761acba270634aaeaa1571bd4a08f7
Roger Silva Souza <rogersilvasouza@hotmail.com>
2016-02-09 21:28:35 -0200

    atualizações
    atualizações

--- app/js/inject.js
+++ app/js/inject.js
@@ -1,21 +1,41 @@
-console.log('injetado');
+/*jslint white: true, browser: true */
+/*globals console */
 
-var elems = document.getElementsByClassName('pane-list-user');
+/**
+* Inject.js
+*
+*/
 
-for (var i = 0; i < elems.length; i++) {
+var i, elems, url, img = 'https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/12670068_934420459968681_3746995988839703274_n.jpg?oh=39c25a41ba70479974155ddd46ee0f33&oe=5765AD9A&__gda__=1462229779_7f2f9354951fb7e4226ba8e11a328bbc';
+
+img = 'https://scontent-mia1-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/942801_1131749893532066_4194837725433874430_n.jpg?oh=20bcbdf081fb48ae1aa8c75a0df3607e&oe=577078EB';
+
+
+elems = document.getElementsByClassName('pane-list-user');
+
+for (i = 0; i < elems.length; i = i + 1) {
     elems[i].style.background = 'red';
 }
 
-var elems = document.getElementsByClassName('message-list');
+function changeBackground() {
 
-var url = decodeURIComponent('https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/12670068_934420459968681_3746995988839703274_n.jpg?oh=39c25a41ba70479974155ddd46ee0f33&oe=5765AD9A&__gda__=1462229779_7f2f9354951fb7e4226ba8e11a328bbc');
+  "use strict";
 
-for (var i = 0; i < elems.length; i++) {
-    elems[i].style.background = "url('" + url + "')";
+  elems = document.getElementsByClassName('pane-chat-body');
+
+  url = decodeURIComponent(img);
+
+  for (i = 0; i < elems.length; i = i + 1) {
+
+    elems[i].style.background       = "url('" + url + "')";
     elems[i].style.backgroundRepeat = 'no-repeat';
+
+  }
+
 }
 
-//https://scontent-gru2-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/1721_1103748099644466_2876796960082427461_n.jpg?oh=544e229c035e6482e3c11181a383185d&oe=57394A29
-//https://scontent-gru2-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/1721_1103748099%…62876796960082427461_n.jpg?oh=544e229c035e6482e3c11181a383185d&oe=57394A29
+changeBackground();
 
-console.log('alterado css');
+document.getElementById("app").addEventListener("click", changeBackground);
+
+console.log('CSS Alterado');
